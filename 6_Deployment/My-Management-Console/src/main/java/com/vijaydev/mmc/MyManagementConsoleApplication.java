0b178d6f2dcde7527f305705328761ef338598aa
Vijay R. Gawade <VijayGawade96@Outlook.com>
2020-02-06 08:45:24 +0530

    extend SpringBootServletInitializer, and override the configure method
    In Spring Boot, to create a WAR for deployment, we need 3 steps:
    
    Extends SpringBootServletInitializer
    Marked the embedded servlet container as provided.
    Update packaging to war

--- 6_Deployment/My-Management-Console/src/main/java/com/vijaydev/mmc/MyManagementConsoleApplication.java
+++ 6_Deployment/My-Management-Console/src/main/java/com/vijaydev/mmc/MyManagementConsoleApplication.java
@@ -2,12 +2,19 @@ package com.vijaydev.mmc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyManagementConsoleApplication {
+public class MyManagementConsoleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyManagementConsoleApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyManagementConsoleApplication.class);
+    }
+	
 }
